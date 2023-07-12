d4ab77b6842fc8bef21ea3cf40fada5d6290c9bc
Kai <kai@Kais-MacBook-Pro.local>
2023-07-12 10:55:13 -0500

    initial login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,30 +2,32 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import TaskDetails from "./pages/TaskDetails";
+import Login from "./pages/Login";
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-  const addTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
+    const addTask = (newTask) => {
+        setTasks([...tasks, newTask]);
+    };
 
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route
-            path="/"
-            element={<Dashboard addTask={addTask} tasks={tasks} />}
-          />
-          <Route
-            path="/tasks/:taskId"
-            element={<TaskDetails tasks={tasks} />}
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Dashboard addTask={addTask} tasks={tasks} />}
+                    />
+                    <Route
+                        path="/tasks/:taskId"
+                        element={<TaskDetails tasks={tasks} />}
+                    />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
