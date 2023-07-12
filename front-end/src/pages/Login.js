ca4903b05e8d4609f8ee2af0ff2e537b94e61d4c
Kai M <maohk0117@gmail.com>
2023-07-12 14:23:19 -0500

    assign the bank end url to a variable

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,54 +3,56 @@ import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function Login(params) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [FAuth, setAuth] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [FAuth, setAuth] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("http://localhost:8000/api/login", {
-        username,
-        password,
-      });
+    const backEndUrl = "http://localhost:8000/api/login";
 
-      const data = response.data;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(backEndUrl, {
+                username,
+                password,
+            });
 
-      if (data.success) {
-        setAuth(true);
-        window.location.href = "/";
-      } else {
-        alert("Invalid username or password.");
-      }
-    } catch (error) {
-      console.error("An error occurred during login:", error);
-    }
-  };
+            const data = response.data;
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="Login" />
-      </form>
-      {setAuth} && (
-      <div>
-        <p>"Email Sent"</p>
-      </div>
-      ){/* <Link to="/register">Register</Link> */}
-    </div>
-  );
+            if (data.success) {
+                setAuth(true);
+                window.location.href = "/";
+            } else {
+                alert("Invalid username or password.");
+            }
+        } catch (error) {
+            console.error("An error occurred during login:", error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input type="submit" value="Login" />
+            </form>
+            {setAuth} && (
+            <div>
+                <p>"Email Sent"</p>
+            </div>
+            )<Link to="/register">Register</Link>
+        </div>
+    );
 }
