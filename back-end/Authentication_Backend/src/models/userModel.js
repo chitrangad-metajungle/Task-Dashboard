af1f6125c3968e58a01b0ddc701493ac7c62b1f3
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-14 17:10:34 -0500

    updated user model to make otp optional

--- back-end/Authentication_Backend/src/models/userModel.js
+++ back-end/Authentication_Backend/src/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  otp: { type: Number, required: true },
+  otp: { type: Number, required: false },
   roles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }],
 });
 
