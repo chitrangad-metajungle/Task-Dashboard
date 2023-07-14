f356971c4e0bb5291b9557030ffc66842989cd89
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-14 18:07:46 -0500

    added component to show loading bar

---
+++ front-end/src/components/Loading.js
@@ -0,0 +1,14 @@
+// Loading component
+
+import React from 'react';
+import '../styles/Loading.css'; // CSS file for styling
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-circle"></div>
+    </div>
+  );
+};
+
+export default Loading;
