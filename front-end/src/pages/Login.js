f582c8bf9008caedb6488c9924479a3903ee7f40
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-14 10:50:53 -0500

    updated login page. added OTP input and 2 factor auth

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,31 +1,77 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import "../styles/styles.css"
 
 export default function Login(params) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [FAuth, setAuth] = useState(false);
+  const [user, setUser] = useState({});
+  const [showLogin, setShowLogin] = useState(true);
 
-  const backEndUrl = "http://localhost:8000/api/login";
+  const [otp, setOtp] = useState('');
+  const [error, setError] = useState('');
+
+  const loginUrl = "http://127.0.0.1:10000/auth/login";
+  const generateOtp = "http://127.0.0.1:10000/auth/generate_two_factor_otp";
+  const validateOtp = "http://127.0.0.1:10000/auth/validate_two_factor_otp";
+
+  const handleOtpChange = (event) => {
+    const inputOtp = event.target.value;
+    if (/^\d{0,6}$/.test(inputOtp)) {
+      setOtp(inputOtp);
+      setError('');
+    } else {
+      setError('Please enter a 6-digit number.');
+    }
+  };
+
+  const handleSubmitOtp = async (event) => {
+    event.preventDefault();
+
+    if (otp.length === 6) {
+        // Perform OTP verification logic
+        console.log('Verifying OTP:', otp);
+        // Call API or perform other actions as needed
+        const generateOtp_response = await axios.post(validateOtp, {
+            username,
+            password,
+            otp
+        });
+        if (generateOtp_response.status == 200) {
+            window.location.href = "/";
+        } else {
+            alert("Failure to generate OTP!");
+        }
+
+    } else {
+        alert("Failure to generate OTP!");
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(backEndUrl, {
+      const login_response = await axios.post(loginUrl, {
         username,
         password,
       });
-      console.log("Calling Login")
-      const data = response.data;
+      console.log(login_response);
 
-      if (data.success) {
+      if (login_response.status == 200) {
         setAuth(true);
-        window.location.href = "/";
+        const generateOtp_response = await axios.post(generateOtp, {
+          username,
+          password,
+        });
+        if (generateOtp_response.status == 200) {
+          setUser(user);
+          setShowLogin(false);
+        } else {
+          alert("Failure to generate OTP!");
+        }
       } else {
         alert("Invalid username or password.");
       }
@@ -35,48 +81,34 @@ export default function Login(params) {
   };
 
   return (
-    // <div class = "d-flex pt-5 justify-content-center">
-      <div class="container">
-        <div class="row d-flex jsutify-content-center mt-5">
-          <div class="col-md-6">
-            <div class="card">
-              <form onSubmit={handleSubmit} class="box">
+    <div>
+      {showLogin ? 
+        <div className="container">
+            <div className="card">
+              <form onSubmit={handleSubmit} className="box">
                 <h1>Login</h1>
-                  <p class="ss">Please Enter your Password!</p>
+                  <p className="ss">Please Enter your Password!</p>
                   <input type="text" name="" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                   <input type="password" name ="" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                  <a class="forgot ss" href="#">Forgot password?</a>
-                  <input type="submit" name="" value="Login" href="#"></input>
-                  <a class="forgot text-info" to="/register" href="#">Register</a>
+                  <a className="forgot ss" href="#">Forgot password?</a>
+                  <input type="submit" className="button" value="Login" href="#"></input>
+                  <a className="forgot text-info" to="/register" href="#">Register</a>
               </form>
-            </div>
-          </div> 
+          </div>
         </div>
-      {/* </div> */}
-
-      {/* <n></n>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="Login" />
-      </form> 
-      <br></br>
-      {setAuth} && (
-      <div>
-        <p>"Email Sent"</p>
-      </div>
-      )<Link to="/register">Register</Link>*/}
+        :
+        <div className="container">
+          <div  className="card">
+              <form onSubmit={handleSubmitOtp} className="box">
+                  <h2>OTP Verification</h2>
+                  <p className="ss">OTP has been sent to your Email!</p>
+                  <input type="text" value={otp} onChange={handleOtpChange} />
+                  {error && <div style={{ color: 'red' }}>{error}</div>}
+                  <button type="submit">Verify OTP</button>
+              </form>
+          </div>
+        </div>
+      }
     </div>
   );
 }
