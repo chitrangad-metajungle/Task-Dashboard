5349c9a58c9c5bb084a386156bd39ef2b3ed69cb
Kai M <maohk0117@gmail.com>
2023-07-17 15:50:57 -0500

    profile page changes

--- front-end/src/components/Profile.js
+++ front-end/src/components/Profile.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "../styles/profile.css";
+import { getCookieValue } from "../Utility/token_functions";
 
 export default function Profile({ closeFunction }) {
   const [username, setUsername] = useState("N/A");
   const [email, setEmail] = useState("N/A");
 
-  async function getUserInfo() {}
+  async function getUserInfo() {
+    getCookieValue("user");
+  }
 
   return (
     <div id="profileContainer">
       <h1>Profile</h1>
-      <div>Username:{username}</div>
-      <div>EmailAddress:{email}</div>
-      <button onClick={closeFunction}>X</button>
+      <div className="profileItem">Username:{username}</div>
+      <div className="profileItem">EmailAddress:{email}</div>
     </div>
   );
 }
