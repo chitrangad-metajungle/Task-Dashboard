837835abfd4f01381027ece78ce8955d7b635cd2
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-07-17 23:05:45 -0500

    updated login/signup pages to follow color scheme

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -14,7 +14,18 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 export default function Login(params) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#18a27a',
+        darker: '#053e85',
+      }
+    },
+  });
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState({});
@@ -138,120 +149,124 @@ export default function Login(params) {
     
     <div>
       {loading && <Loading />}
-      {showLogin ? 
-        <Container component="main" maxWidth="sm">
-          <Box
-            sx={{
-              boxShadow: 3,
-              borderRadius: 2,
-              px: 4,
-              py: 6,
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-                value={username} 
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {error && <div style={{ color: 'red' }}>{error}</div>}
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+      <ThemeProvider theme={theme}>
+        {showLogin ? 
+          <Container component="main" maxWidth="sm">
+            <Box
+              sx={{
+                boxShadow: 3,
+                borderRadius: 2,
+                px: 4,
+                py: 6,
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  theme={theme} 
+                  color="primary"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                  value={username} 
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-        :
-        <Container component="main" maxWidth="sm">
-          <Box
-            sx={{
-              boxShadow: 3,
-              borderRadius: 2,
-              px: 4,
-              py: 6,
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-            OTP Verification
-            </Typography>
-            <Box component="form" onSubmit={handleSubmitOtp} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="otp"
-                label="OTP"
-                name="otp"
-                autoComplete="otp"
-                autoFocus
-                value={otp} 
-                onChange={handleOtpChange}
-              />
-              {error && <div style={{ color: 'red' }}>{error}</div>}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                disabled={!enableOtpButton}
-              >
-                Sign In
-              </Button>
+          </Container>
+          :
+          <Container component="main" maxWidth="sm">
+            <Box
+              sx={{
+                boxShadow: 3,
+                borderRadius: 2,
+                px: 4,
+                py: 6,
+                marginTop: 8,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h5">
+              OTP Verification
+              </Typography>
+              <Box component="form" onSubmit={handleSubmitOtp} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="otp"
+                  label="OTP"
+                  name="otp"
+                  autoComplete="otp"
+                  autoFocus
+                  value={otp} 
+                  onChange={handleOtpChange}
+                />
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={!enableOtpButton}
+                >
+                  Sign In
+                </Button>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-      }
+          </Container>
+        }
+      </ThemeProvider>
     </div>
   );
 }
